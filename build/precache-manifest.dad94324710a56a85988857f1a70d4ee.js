d19d673d432c50d07804047825fe158aadfffe64
matenko10 <valatkamatas@gmail.com>
2021-04-06 18:39:12 +0300

    first

---
+++ build/precache-manifest.dad94324710a56a85988857f1a70d4ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5619398e3475acef9bd7886072e45d6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "525249d37b3dbbe9d84a",
+    "url": "/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "/static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "525249d37b3dbbe9d84a",
+    "url": "/static/js/main.6ca1383c.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
